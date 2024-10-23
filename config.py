278b0afda6428394d2601044f733d3e331b7f98f
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-10-23 19:09:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://nshubh345:1FmseyW0TKaWNMNo@clus
 DB_NAME = os.environ.get("DB_NAME", "filesharexbot")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002248026515"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002394197305"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
